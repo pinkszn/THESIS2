93a3d9662ad9456af94295699a0bb1007bdbe585
ULTIMATUM9000 <kylepogideleon@gmail.com>
2021-05-24 16:04:18 +0800

    Added variety of code
    -BGMManager
    -AudioManager
    -ItemPickup
    -ItemManager (Crafting System)
    -Singleton
    -ObjectPooling

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+	public Sound[] sounds;
+	void Awake()
+	{
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.playOnAwake = s.playOnAwake;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+		}
+	}
+
+	public void Play(string name)
+	{
+		Sound s = Array.Find(sounds, sound => sound.name == name);
+		s.source.Play();
+	}
+
+	public void Stop(string name)
+	{
+		Sound s = Array.Find(sounds, sound => sound.name == name);
+
+		if (!s.source.enabled)
+		{
+			return;
+		}
+		else
+		{
+			s.source.Stop();
+		}
+
+	}
+}
+
