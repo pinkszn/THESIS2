a9159790592d6ac663073df2fb60442bd1dd127f
JK <lexuskrav@gmail.com>
2021-06-08 11:45:45 +0800

    Button Script for UI

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    #region Recycling Compound Buttons
+    void SeperateButton()
+    {
+        /*
+         * take seperated item out of player's inventory
+         * add new materials to player's inventory
+        */
+    }
+    void CrushButton()
+    {
+        /*
+         * 
+        */
+    }
+    void WashButton()
+    {
+        /*
+         * 
+         * 
+        */
+    }
+    void ShredButton()
+    {
+        /*
+         * 
+         * 
+         */
+    }
+    void TrashButton()
+    {
+
+    }
+    void ReuseButton()
+    {
+
+    }
+    void CodexButton()
+    {
+
+    }
+    #endregion
+
+    #region EndScreen Buttons()
+    void RecycleButton()
+    {
+
+    }
+    void ContinueButton()
+    {
+
+    }
+    #endregion
+
+    #region Crafting Buttons
+    void CraftBaseball() { }
+    #endregion
+}
