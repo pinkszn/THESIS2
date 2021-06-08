e4a427973167925b833356fbd84a720782334043
JK <lexuskrav@gmail.com>
2021-06-08 14:24:18 +0800

    Added Items in Item Manager and updated UI Data Variables

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -11,8 +11,12 @@ public class ItemManager : Singleton<ItemManager>
     [SerializeField] TextMeshProUGUI[] CraftingText; //UI for values of crafting
     [SerializeField] TextMeshProUGUI[] ItemText; // UI for current values of usable items
 
-    public int Plastic; //eto yung mga amount ng raw materials na napipickup
-    public int item02; //names will change depending sa pangalan ng mga raw materials (design team)
+    public int plastic;
+    public int aluminum;
+    public int recycledPlastic;
+    public int recycledAluminum;
+    public int ruinedPlastic;
+    public int ruinedAluminum; 
 
     public int EcoBricks; //amount of the crafted usable items, yung mga eco bombs and stuff, dagdag nlang 
     public int Carpet;
@@ -28,21 +32,22 @@ public class ItemManager : Singleton<ItemManager>
 	public void InitializeData() //call this function when starting a level
 	{
         //item01 = PlayerPrefs.GetInt("Item01");
-	}
+        plastic = PlayerPrefs.GetInt("plastic");
+        aluminum = PlayerPrefs.GetInt("aluminum");
+    }
 
     //name of function subject to change
     public void CraftEcoBricks() //eto yung icoconect dun sa button UI ng pag craft //copy paste nlang buong function pag mayroon pang ibang recipes
 	{
         //if statement ng amount of raw materials na kelangan sa pag craft ng item na ito
-        if (Plastic > 1) //values subject to change
+        if (plastic > 1) //values subject to change
         {
-            Plastic -= 1;
+            recycledPlastic -= 2;
             EcoBricks += 1;
             return;
         }
         else
             return;
-
         //if kulang ng materials, disable button or just do nothing
         //if enough materials, add one to the amount of craftedItem amount, tas bawasan ang raw materials
 	}
