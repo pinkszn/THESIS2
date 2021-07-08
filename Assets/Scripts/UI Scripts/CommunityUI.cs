02db5f39d9ffaa4f489279fdcfc6c3b32cf6c0f4
ULTIMATUM9000 <kylepogideleon@gmail.com>
2021-07-09 00:12:45 +0800

    Community UI Updates

--- Assets/Scripts/UI Scripts/CommunityUI.cs
+++ Assets/Scripts/UI Scripts/CommunityUI.cs
@@ -1,11 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CommunityUI : MonoBehaviour
 {
-    #region Community Screen Button Functions
-    public void DonateEcoBrick()//Hard Code Muna
+    [SerializeField] private Button[] donateButtons;
+
+    [SerializeField] private Slider[] donatedAmmount;
+
+	private void Update()
+    {
+        UpdateCommunityBarValue();
+        ButtonFunctionality();
+    }
+
+    void UpdateCommunityBarValue()
+	{
+        donatedAmmount[0].value = ItemManager.instance.DonatedEcoBricksAmount; //Donated Eco Brick 
+        donatedAmmount[1].value = ItemManager.instance.DonatedCarpetAmount; //Donated Carpet
+        donatedAmmount[2].value = ItemManager.instance.DonatedBaseballBat; //Donated Base Ball Bat 
+        donatedAmmount[3].value = ItemManager.instance.DonatedSkateBoardAmount; //Donated Skate Board 
+	}
+
+    void ButtonFunctionality()
+	{
+        if(ItemManager.instance.DonatedEcoBricksAmount == ItemManager.instance.MaxDonatedEcoBricksAmount)
+		{
+            donateButtons[0].interactable = false;
+		}
+        if(ItemManager.instance.DonatedEcoBricksAmount != ItemManager.instance.MaxDonatedEcoBricksAmount)
+		{
+            donateButtons[0].interactable = true;
+		}
+        
+        if(ItemManager.instance.DonatedCarpetAmount == ItemManager.instance.MaxDonatedCarpetAmount)
+		{
+            donateButtons[1].interactable = false;
+		}
+        if(ItemManager.instance.DonatedCarpetAmount != ItemManager.instance.MaxDonatedCarpetAmount)
+		{
+            donateButtons[1].interactable = true;
+		}
+        
+        if(ItemManager.instance.DonatedBaseballBat == ItemManager.instance.MaxDonatedBaseballBat)
+		{
+            donateButtons[2].interactable = false;
+		}
+        if(ItemManager.instance.DonatedBaseballBat != ItemManager.instance.MaxDonatedBaseballBat)
+		{
+            donateButtons[2].interactable = true;
+		}
+
+        if (ItemManager.instance.DonatedSkateBoardAmount == ItemManager.instance.MaxDonatedSkateBoardAmount)
+        {
+            donateButtons[3].interactable = false;
+        }
+        if (ItemManager.instance.DonatedSkateBoardAmount != ItemManager.instance.MaxDonatedSkateBoardAmount)
+        {
+            donateButtons[3].interactable = true;
+        }
+    }
+
+
+
+
+	#region Community Screen Button Functions
+	public void DonateEcoBrick()//Hard Code Muna
     {
         if (ItemManager.instance.EcoBricks > 20)
         {
